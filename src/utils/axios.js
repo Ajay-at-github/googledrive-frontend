141d0723effdb40f6534f256250225e4689fe483
Ajay-at-github <73772218103@ksrct.net>
2026-02-06 12:03:51 +0530

    updated axios and pkg.json

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://Ajay-at-github.github.io/googledrive-backend/api/v1",
+  // baseURL: "https://ajay-at-github.github.io/googledrive-backend/api/v1",
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
